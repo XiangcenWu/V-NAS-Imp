fab6cad18351ae9fc66c92deac358ae99986e199
XiangcenWu <93538241+XiangcenWu@users.noreply.github.com>
2022-05-14 00:12:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 from monai.inferers import sliding_window_inference
 from monai.data import decollate_batch
 from monai.metrics import DiceMetric
+from monai.losses import DiceCELoss
 from monai.data import (
     DataLoader,
     CacheDataset,
@@ -111,16 +112,11 @@ val_transforms = Compose(
 
 
 # load the 
-Train_datalist = load_decathlon_datalist("./data/dataset.json", True, "training")
-Val_datalist = load_decathlon_datalist("./data/dataset.json", True, "val")
+Train_datalist = load_decathlon_datalist("./P_segmentation/Task07_Pancreas/dataset.json", True, "training")
+Val_datalist = load_decathlon_datalist("./P_segmentation/Task07_Pancreas/dataset.json", True, "val")
 
 
-train_ds = CacheDataset(
-    data=Train_datalist,
-    transform=train_transforms,
-    cache_num=4,
 
-)
 train_ds = CacheDataset(
     data=Train_datalist,
     transform=train_transforms,
@@ -135,23 +131,23 @@ val_ds = CacheDataset(
     data=Val_datalist, transform=val_transforms, cache_num=1, cache_rate=1.0, num_workers=4
 )
 val_loader = DataLoader(
-    val_ds, batch_size=1, shuffle=False, num_workers=4, pin_memory=True
+    val_ds, batch_size=2, shuffle=False, num_workers=4, pin_memory=True
 )
-gpu_idx = 0
+gpu_idx = 2
 if torch.cuda.is_available():
     device = 'cuda:' + str(gpu_idx)
 else:
     device = 'cpu'
 
 
-post_label = AsDiscrete(to_onehot=14)
-post_pred = AsDiscrete(argmax=True, to_onehot=14)
+post_label = AsDiscrete(to_onehot=3)
+post_pred = AsDiscrete(argmax=True, to_onehot=3)
 dice_metric = DiceMetric(include_background=True, reduction="mean", get_not_nans=False)
 
 
 
 
-model = Network(1)
+model = Network(3).to(device)
 def validation(val_loader):
     model.eval()
     with torch.no_grad():
@@ -168,4 +164,50 @@ def validation(val_loader):
         dice_metric.reset()
     return mean_dice_val
 
-print(validation(val_loader))
+print(validation(val_loader=val_loader))
+
+
+
+eval_num = 1
+max_iterations = 10
+loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
+torch.backends.cudnn.benchmark = True
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+def train(global_step, train_loader, dice_val_best):
+    model.train()
+    epoch_loss = 0
+    
+    step = 0
+    for step, batch in enumerate(train_loader):
+        step += 1
+        x, y = batch["image"].to(device), batch["label"].to(device)
+        print("x, y ")
+        o = model(x)
+        print("o.shape: {}".format(o.shape))
+        print()
+        loss = loss_function(o, y)
+        loss.backward()
+        epoch_loss += loss.item()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        dice_val = validation(val_loader)
+        if dice_val > dice_val_best:
+            dice_val_best = dice_val
+            torch.save(
+                model.state_dict(), "./model/best_metric_model.pth"
+            )
+            print("Model Was Saved !")
+    global_step += 1
+
+    return global_step, dice_val_best
+
+max_iterations = 10
+eval_num = 1
+dice_val_best = 0
+
+global_step = 0
+while global_step < max_iterations:
+    global_step, dice_val_best = train(global_step, train_loader, dice_val_best)
+    print("At global step{}, dice_val_best {}".format(global_step, dice_val_best))
+
